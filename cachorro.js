62a9366b9fef776f5fa6972ee03ed610d8d751e9
Isaque1267 <isaqueferreiraqueiroz@gmail.com>
2026-06-22 21:54:46 -0300

    tudo que eu ja tinha feito e mostrado pro coutinho

---
+++ cachorro.js
@@ -0,0 +1,90 @@
+const linkAtual = window.location.href;
+const itensMenu = document.querySelectorAll('.paginas a');
+
+itensMenu.forEach(link => {
+  if (linkAtual.endsWith(link.getAttribute('href'))) {
+    link.classList.add('ativo');
+  }
+});
+
+const osso = document.getElementById('item-arremesso');
+let segurando = false;
+let posX = 100, posY = 100;
+let velX = 0, velY = 0;
+let lastMouseX, lastMouseY;
+const gravidade = 1;
+
+let angulo = 0;
+let velAngulo = 0;
+
+osso.addEventListener('mousedown', (e) => {
+  segurando = true;
+  velX = 0; 
+  velY = 0;
+  velAngulo = 0;
+});
+
+window.addEventListener('mousemove', (e) => {
+  if (segurando) {
+    velX = e.clientX - lastMouseX;
+    velY = e.clientY - lastMouseY;
+    
+    velAngulo = velX * 0.5;
+
+    posX = e.clientX - 60;
+    posY = e.clientY - 30;
+    
+    angulo += velAngulo;
+    atualizarPosicao();
+  }
+  lastMouseX = e.clientX;
+  lastMouseY = e.clientY;
+});
+
+window.addEventListener('mouseup', () => {
+  segurando = false;
+});
+
+function animar() {
+  if (!segurando) {
+    velY += gravidade;
+    posX += velX;
+    posY += velY;
+    
+    angulo += velAngulo;
+
+    velX *= 0.99;
+    velY *= 0.99;
+    velAngulo *= 0.98;
+
+    if (posX + 120 > window.innerWidth || posX < 0) {
+        velX *= -0.7;
+        velAngulo = velY * 0.5;
+        posX = posX < 0 ? 0 : window.innerWidth - 120;
+    }
+    
+    if (posY + 60 > window.innerHeight) {
+        velY *= -0.6;
+        velAngulo = velX * 0.5;
+        posY = window.innerHeight - 60;
+        if (Math.abs(velY) < 2) {
+            velY = 0;
+            velAngulo *= 0.9;
+        }
+    } else if (posY < 0) {
+        velY *= -0.7;
+        velAngulo = velX * 0.5;
+        posY = 0;
+    }
+    atualizarPosicao();
+  }
+  requestAnimationFrame(animar);
+}
+
+function atualizarPosicao() {
+  osso.style.left = posX + 'px';
+  osso.style.top = posY + 'px';
+  osso.style.transform = `rotate(${angulo}deg)`;
+}
+
+animar();
